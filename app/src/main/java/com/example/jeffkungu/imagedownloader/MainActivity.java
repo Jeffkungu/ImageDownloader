ff789c8eb03c21fd599a5814eecfc23999f09984
Jeffkungu <Jeffkungu@gmail.com>
2018-03-19 13:49:24 +0300

    [Feature] adds a class that implements a Runnable to create a background thread for image download. Also updates the view containing the progressbar using the runOnUiThread method.

--- app/src/main/java/com/example/jeffkungu/imagedownloader/MainActivity.java
+++ app/src/main/java/com/example/jeffkungu/imagedownloader/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.jeffkungu.imagedownloader;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.EditText;
@@ -9,8 +12,19 @@ import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.ProgressBar;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import static android.view.View.VISIBLE;
+
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
 
+    private String INPUT_STREAM = "INPUT_STREAM: ";
     private EditText editText;
     private ListView listView;
     private String[] listOfImages;
@@ -26,6 +40,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         listView.setOnItemClickListener(this);
         listOfImages = getResources().getStringArray(R.array.imageUrls);
         progressBar = findViewById(R.id.downloadProgress);
+        loadingSection = findViewById(R.id.loadingSection);
     }
 
     @Override
@@ -34,5 +49,86 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     }
 
     public void downloadImage(View view) {
+        String url = editText.getText().toString();
+        DownloadImagesThread myThread = new DownloadImagesThread(url);
+        Thread thread = new Thread(myThread);
+        thread.start();
+    }
+
+    public boolean downloadImageUsingThreads(String url) {
+        boolean successful = false;
+        URL downloadUrl;
+        HttpURLConnection connection = null;
+        InputStream inputStream = null;
+        FileOutputStream fileOutputStream = null;
+        File file;
+        try {
+            downloadUrl = new URL(url);
+            connection = (HttpURLConnection) downloadUrl.openConnection();
+            inputStream = connection.getInputStream();
+
+            file = new File(Environment.getExternalStoragePublicDirectory(Environment.
+                    DIRECTORY_PICTURES).getAbsolutePath() + "/" + Uri.parse(url).getLastPathSegment());
+            Log.d("URL_PATH", " " + file.getAbsolutePath());
+            fileOutputStream = new FileOutputStream(file);
+
+            int read;
+            byte[] buffer = new byte[1024];
+            while ((read=inputStream.read(buffer))!=-1) {
+                Log.d(INPUT_STREAM, INPUT_STREAM + read);
+                fileOutputStream.write(buffer, 0, read);
+            }
+            successful = true;
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        finally {
+            this.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    loadingSection.setVisibility(View.GONE);
+                }
+            });
+            if (connection!=null) {
+                connection.disconnect();
+            }
+            if (inputStream!=null) {
+                try {
+                    inputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (fileOutputStream!=null) {
+                try {
+                    fileOutputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return successful;
+    }
+
+    class DownloadImagesThread implements Runnable {
+
+        private String url;
+
+        public DownloadImagesThread(String url) {
+            this.url = url;
+        }
+
+        @Override
+        public void run() {
+            MainActivity.this.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    loadingSection.setVisibility(VISIBLE);
+                }
+            });
+            downloadImageUsingThreads(url);
+        }
     }
 }
